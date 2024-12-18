43b060298b877411c53441508d6b56410d081141
aLearningLad <mocmanca@gmail.com>
2024-12-18 22:23:56 +0200

    addition of dummy widget to test hot reloading

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text("This is the app bar! Yeah!")),
+          body: DummyWidget())));
+}
+
+class DummyWidget extends StatelessWidget {
+  const DummyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text(
+          "This is a stateless widget with a build method. So hot-reload should work!"),
+    );
+  }
 }
